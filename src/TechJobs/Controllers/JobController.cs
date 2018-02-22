5a26d01c0ff2db0e3095f9a51791cab6e1fbd435
shiquann <shiquann.j@gmail.com>
2018-02-21 21:10:47 -0600

    Made all 7 changes

--- src/TechJobs/Controllers/JobController.cs
+++ src/TechJobs/Controllers/JobController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TechJobs.Data;
+using TechJobs.Models;
 using TechJobs.ViewModels;
 
 namespace TechJobs.Controllers
@@ -19,8 +20,8 @@ namespace TechJobs.Controllers
         public IActionResult Index(int id)
         {
             // TODO #1 - get the Job with the given ID and pass it into the view
-
-            return View();
+            Job someJob = jobData.Find(id);
+            return View(someJob);
         }
 
         public IActionResult New()
@@ -35,7 +36,20 @@ namespace TechJobs.Controllers
             // TODO #6 - Validate the ViewModel and if valid, create a 
             // new Job and add it to the JobData data store. Then
             // redirect to the Job detail (Index) action/view for the new Job.
+            if (ModelState.IsValid)
+            {
+                Job newJob = new Job{
+                    Name = newJobViewModel.Name,
+                    Employer = jobData.Employers.Find(newJobViewModel.EmployerID),
+                    Location = jobData.Locations.Find(newJobViewModel.LocationID),
+                    CoreCompetency = jobData.CoreCompetencies.Find(newJobViewModel.CoreCompetenciesID),
+                    PositionType = jobData.PositionTypes.Find(newJobViewModel.PosititionTypesID)
 
+                };
+                jobData.Jobs.Add(newJob);
+                return Redirect("/Job?id=" + newJob.ID);
+            }
+          
             return View(newJobViewModel);
         }
     }
--- src/TechJobs/ViewModels/SearchJobsViewModel.cs
+++ src/TechJobs/ViewModels/SearchJobsViewModel.cs
@@ -5,7 +5,7 @@ using TechJobs.Models;
 
 namespace TechJobs.ViewModels
 {
-    public class SearchJobsViewModel 
+    public class SearchJobsViewModel : BaseViewModel
     {
         // TODO #7.1 - Extract members common to JobFieldsViewModel
         // to BaseViewModel
@@ -21,10 +21,8 @@ namespace TechJobs.ViewModels
         public string Value { get; set; } = "";
 
         // All columns, for display
-        public List<JobFieldType> Columns { get; set; }
 
         // View title
-        public string Title { get; set; } = "";
 
         public SearchJobsViewModel()
         {
